67aa8f0bd31f9b752a25794d718d088730808d87
Athlan <athlan@vgroup.pl>
2018-08-14 22:26:30 +0200

    Datepicker specification.

--- test/integration/suites/DatepickerFormatter.php
+++ test/integration/suites/DatepickerFormatter.php
@@ -33,4 +33,84 @@ class DatepickerFormatter extends BaseTestCase {
 		// when & then.
 		$this->permalink_asserter->has_permalink( $created_post_id, '/2018-07-21/some-page-title/' );
 	}
+
+	/**
+	 * Test case.
+	 */
+	function test_generates_permalink_with_custom_format() {
+		// given.
+		$this->permalink_steps->given_permalink_structure( '/%field_some_datepicker_field(format="d-m-y")%/%postname%/' );
+		$this->acf_steps->given_acf_field('some_datepicker_field', 'date_picker');
+
+		$post_params = array(
+			'post_title' => 'Some page title',
+			'meta_input' => array(
+				'some_datepicker_field' => '20180721',
+			)
+		);
+		$created_post_id = $this->factory()->post->create( $post_params );
+
+		// when & then.
+		$this->permalink_asserter->has_permalink( $created_post_id, '/21-07-18/some-page-title/' );
+	}
+
+	/**
+	 * Test case.
+	 */
+	function test_generates_permalink_with_year_only() {
+		// given.
+		$this->permalink_steps->given_permalink_structure( '/%field_some_datepicker_field(year)%/%postname%/' );
+		$this->acf_steps->given_acf_field('some_datepicker_field', 'date_picker');
+
+		$post_params = array(
+			'post_title' => 'Some page title',
+			'meta_input' => array(
+				'some_datepicker_field' => '20180721',
+			)
+		);
+		$created_post_id = $this->factory()->post->create( $post_params );
+
+		// when & then.
+		$this->permalink_asserter->has_permalink( $created_post_id, '/2018/some-page-title/' );
+	}
+
+	/**
+	 * Test case.
+	 */
+	function test_generates_permalink_with_month_only() {
+		// given.
+		$this->permalink_steps->given_permalink_structure( '/%field_some_datepicker_field(month)%/%postname%/' );
+		$this->acf_steps->given_acf_field('some_datepicker_field', 'date_picker');
+
+		$post_params = array(
+			'post_title' => 'Some page title',
+			'meta_input' => array(
+				'some_datepicker_field' => '20180721',
+			)
+		);
+		$created_post_id = $this->factory()->post->create( $post_params );
+
+		// when & then.
+		$this->permalink_asserter->has_permalink( $created_post_id, '/07/some-page-title/' );
+	}
+
+	/**
+	 * Test case.
+	 */
+	function test_generates_permalink_with_day_only() {
+		// given.
+		$this->permalink_steps->given_permalink_structure( '/%field_some_datepicker_field(day)%/%postname%/' );
+		$this->acf_steps->given_acf_field('some_datepicker_field', 'date_picker');
+
+		$post_params = array(
+			'post_title' => 'Some page title',
+			'meta_input' => array(
+				'some_datepicker_field' => '20180721',
+			)
+		);
+		$created_post_id = $this->factory()->post->create( $post_params );
+
+		// when & then.
+		$this->permalink_asserter->has_permalink( $created_post_id, '/21/some-page-title/' );
+	}
 }
