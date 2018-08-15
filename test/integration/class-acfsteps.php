4ba2140ac604656186caa297e25f49d08d00f872
Athlan <athlan@vgroup.pl>
2018-08-15 10:48:58 +0200

    phpcs

--- test/integration/class-acfsteps.php
+++ test/integration/class-acfsteps.php
@@ -21,53 +21,52 @@ class AcfSteps {
 	 *
 	 * @param string $name The name of field.
 	 * @param string $type The type of field.
-	 * @param array $field_options Additional field options.
-	 * @param array $group_options Additional field group options.
+	 * @param array  $field_options Additional field options.
+	 * @param array  $group_options Additional field group options.
 	 */
-	public function given_acf_field($name, $type, array $field_options = array(), $group_options = array()) {
-		$random = md5(microtime());
-		$random = substr($random, 0, 13);
+	public function given_acf_field( $name, $type, array $field_options = array(), $group_options = array() ) {
+		$random = md5( microtime() );
+		$random = substr( $random, 0, 13 );
 
 		$group_options_defaults = array(
-			'id' => 'acf_' . $random,
-			'title' => 'For ' . $name . ' field',
-			'location' => array (
-				array (
-					array (
-						'param' => 'post_type',
+			'id'         => 'acf_' . $random,
+			'title'      => 'For ' . $name . ' field',
+			'location'   => array(
+				array(
+					array(
+						'param'    => 'post_type',
 						'operator' => '==',
-						'value' => 'post',
+						'value'    => 'post',
 						'order_no' => 0,
 						'group_no' => 0,
 					),
 				),
 			),
-			'options' => array (
-				'position' => 'normal',
-				'layout' => 'no_box',
-				'hide_on_screen' => array (
-				),
+			'options'    => array(
+				'position'       => 'normal',
+				'layout'         => 'no_box',
+				'hide_on_screen' => array(),
 			),
 			'menu_order' => 0,
 		);
 
 		$field_options_defaults = array(
-			'key' => 'field_' . $name,
-			'label' => $name,
-			'name' => $name,
-			'type' => $type,
+			'key'           => 'field_' . $name,
+			'label'         => $name,
+			'name'          => $name,
+			'type'          => $type,
 			'default_value' => '',
-			'placeholder' => '',
-			'maxlength' => '',
-			'rows' => '',
-			'formatting' => 'br',
+			'placeholder'   => '',
+			'maxlength'     => '',
+			'rows'          => '',
+			'formatting'    => 'br',
 		);
 
 		$final_group_options = $group_options + $group_options_defaults;
 		$final_field_options = $field_options + $field_options_defaults;
 
-		$final_group_options['fields'] = array($final_field_options);
+		$final_group_options['fields'] = array( $final_field_options );
 
-		register_field_group($final_group_options);
+		register_field_group( $final_group_options );
 	}
 }
--- test/integration/suites/DatepickerFormatter.php
+++ test/integration/suites/DatepickerFormatter.php
@@ -20,13 +20,13 @@ class DatepickerFormatter extends BaseTestCase {
 	function test_generates_permalink_with_default_date_format() {
 		// given.
 		$this->permalink_steps->given_permalink_structure( '/%field_some_datepicker_field%/%postname%/' );
-		$this->acf_steps->given_acf_field('some_datepicker_field', 'date_picker');
+		$this->acf_steps->given_acf_field( 'some_datepicker_field', 'date_picker' );
 
-		$post_params = array(
+		$post_params     = array(
 			'post_title' => 'Some page title',
 			'meta_input' => array(
 				'some_datepicker_field' => '20180721',
-			)
+			),
 		);
 		$created_post_id = $this->factory()->post->create( $post_params );
 
@@ -40,13 +40,13 @@ class DatepickerFormatter extends BaseTestCase {
 	function test_generates_permalink_with_custom_format() {
 		// given.
 		$this->permalink_steps->given_permalink_structure( '/%field_some_datepicker_field(format="d-m-y")%/%postname%/' );
-		$this->acf_steps->given_acf_field('some_datepicker_field', 'date_picker');
+		$this->acf_steps->given_acf_field( 'some_datepicker_field', 'date_picker' );
 
-		$post_params = array(
+		$post_params     = array(
 			'post_title' => 'Some page title',
 			'meta_input' => array(
 				'some_datepicker_field' => '20180721',
-			)
+			),
 		);
 		$created_post_id = $this->factory()->post->create( $post_params );
 
@@ -60,13 +60,13 @@ class DatepickerFormatter extends BaseTestCase {
 	function test_generates_permalink_with_year_only() {
 		// given.
 		$this->permalink_steps->given_permalink_structure( '/%field_some_datepicker_field(year)%/%postname%/' );
-		$this->acf_steps->given_acf_field('some_datepicker_field', 'date_picker');
+		$this->acf_steps->given_acf_field( 'some_datepicker_field', 'date_picker' );
 
-		$post_params = array(
+		$post_params     = array(
 			'post_title' => 'Some page title',
 			'meta_input' => array(
 				'some_datepicker_field' => '20180721',
-			)
+			),
 		);
 		$created_post_id = $this->factory()->post->create( $post_params );
 
@@ -80,13 +80,13 @@ class DatepickerFormatter extends BaseTestCase {
 	function test_generates_permalink_with_month_only() {
 		// given.
 		$this->permalink_steps->given_permalink_structure( '/%field_some_datepicker_field(month)%/%postname%/' );
-		$this->acf_steps->given_acf_field('some_datepicker_field', 'date_picker');
+		$this->acf_steps->given_acf_field( 'some_datepicker_field', 'date_picker' );
 
-		$post_params = array(
+		$post_params     = array(
 			'post_title' => 'Some page title',
 			'meta_input' => array(
 				'some_datepicker_field' => '20180721',
-			)
+			),
 		);
 		$created_post_id = $this->factory()->post->create( $post_params );
 
@@ -100,13 +100,13 @@ class DatepickerFormatter extends BaseTestCase {
 	function test_generates_permalink_with_day_only() {
 		// given.
 		$this->permalink_steps->given_permalink_structure( '/%field_some_datepicker_field(day)%/%postname%/' );
-		$this->acf_steps->given_acf_field('some_datepicker_field', 'date_picker');
+		$this->acf_steps->given_acf_field( 'some_datepicker_field', 'date_picker' );
 
-		$post_params = array(
+		$post_params     = array(
 			'post_title' => 'Some page title',
 			'meta_input' => array(
 				'some_datepicker_field' => '20180721',
-			)
+			),
 		);
 		$created_post_id = $this->factory()->post->create( $post_params );
 
