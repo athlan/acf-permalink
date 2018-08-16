80dfbae4757d4d00951625826d6c574b7333feb0
Athlan <athlan@vgroup.pl>
2018-08-16 23:15:43 +0200

    Download 3rd party plugins from github

--- bin/run-tests-before.sh
+++ bin/run-tests-before.sh
@@ -2,17 +2,26 @@
 
 install_plugin() {
     plugin_name=$1
+    download_url=$2
+
+    if [ -z "$download_url" ]; then
+        download_url="https://downloads.wordpress.org/plugin/$plugin_name.zip"
+    fi;
 
     mkdir -p target/plugins
 
-    [ -d "target/plugins/$1/" ] && return;
+    [ -d "target/plugins/$plugin_name/" ] && return;
 
-    rm target/plugins/$1.zip
-    rm -r target/plugins/$1/
+    rm target/plugins/$plugin_name.zip
+    rm -r target/plugins/$plugin_name/
 
-    curl https://downloads.wordpress.org/plugin/$1.zip > target/plugins/$1.zip
-    unzip target/plugins/$1.zip -d target/plugins
+    curl -L $download_url > target/plugins/$plugin_name.zip
+    unzip target/plugins/$plugin_name.zip -d target/plugins/tmp
+
+    mkdir target/plugins/$plugin_name/
+    cp -r target/plugins/tmp/*/* target/plugins/$plugin_name/
+    rm -rf target/plugins/tmp
 }
 
 install_plugin advanced-custom-fields
-install_plugin custom-fields-permalink-redux
+install_plugin custom-fields-permalink-redux https://github.com/athlan/wordpress-custom-fields-permalink-plugin/archive/feature/permalink-attributes.zip
