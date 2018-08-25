822cf4dfb1a991fc2ad51ff06b075ae7ea4251c1
Athlan <athlan@vgroup.pl>
2018-08-25 13:34:07 +0200

    phpcs

--- test/unit/Field_Permalink_Formatter_Context_UnitTest.php
+++ test/unit/Field_Permalink_Formatter_Context_UnitTest.php
@@ -25,16 +25,18 @@ class Field_Permalink_Formatter_Context_UnitTest extends PHPUnit_Framework_TestC
 		$context = new Field_Permalink_Formatter_Context();
 
 		// when.
-		$context->initialize($this->field_name, $this->field_value, $this->field_original_value,
-			$this->permalink_options, $this->acf_field_options, $this->wp_post);
+		$context->initialize(
+			$this->field_name, $this->field_value, $this->field_original_value,
+			$this->permalink_options, $this->acf_field_options, $this->wp_post
+		);
 
 		// then.
-		$this->assertEquals( $this->field_name, $context->field_name());
-		$this->assertEquals( $this->field_value, $context->value());
-		$this->assertEquals( $this->field_original_value, $context->value_original());
-		$this->assertEquals( $this->permalink_options, $context->permalink_options());
-		$this->assertEquals( $this->acf_field_options, $context->acf_field_options());
-		$this->assertEquals( $this->wp_post, $context->post());
+		$this->assertEquals( $this->field_name, $context->field_name() );
+		$this->assertEquals( $this->field_value, $context->value() );
+		$this->assertEquals( $this->field_original_value, $context->value_original() );
+		$this->assertEquals( $this->permalink_options, $context->permalink_options() );
+		$this->assertEquals( $this->acf_field_options, $context->acf_field_options() );
+		$this->assertEquals( $this->wp_post, $context->post() );
 	}
 
 	/**
@@ -45,11 +47,13 @@ class Field_Permalink_Formatter_Context_UnitTest extends PHPUnit_Framework_TestC
 		$context = new Field_Permalink_Formatter_Context();
 
 		// when.
-		$context->initialize($this->field_name, $this->field_value, $this->field_original_value,
-			$this->permalink_options, $this->acf_field_options, $this->wp_post);
+		$context->initialize(
+			$this->field_name, $this->field_value, $this->field_original_value,
+			$this->permalink_options, $this->acf_field_options, $this->wp_post
+		);
 
 		// then.
-		$this->assertFalse( $context->is_terminated());
+		$this->assertFalse( $context->is_terminated() );
 	}
 
 	/**
@@ -60,13 +64,15 @@ class Field_Permalink_Formatter_Context_UnitTest extends PHPUnit_Framework_TestC
 		$context = new Field_Permalink_Formatter_Context();
 
 		// when.
-		$context->initialize($this->field_name, $this->field_value, $this->field_original_value,
-			$this->permalink_options, $this->acf_field_options, $this->wp_post);
+		$context->initialize(
+			$this->field_name, $this->field_value, $this->field_original_value,
+			$this->permalink_options, $this->acf_field_options, $this->wp_post
+		);
 
 		$context->terminate();
 
 		// then.
-		$this->assertTrue( $context->is_terminated());
+		$this->assertTrue( $context->is_terminated() );
 	}
 
 	/**
@@ -77,16 +83,20 @@ class Field_Permalink_Formatter_Context_UnitTest extends PHPUnit_Framework_TestC
 		$context = new Field_Permalink_Formatter_Context();
 
 		// when.
-		$context->initialize($this->field_name, $this->field_value, $this->field_original_value,
-			$this->permalink_options, $this->acf_field_options, $this->wp_post);
+		$context->initialize(
+			$this->field_name, $this->field_value, $this->field_original_value,
+			$this->permalink_options, $this->acf_field_options, $this->wp_post
+		);
 
 		$context->terminate();
 
-		$context->initialize($this->field_name, $this->field_value, $this->field_original_value,
-			$this->permalink_options, $this->acf_field_options, $this->wp_post);
+		$context->initialize(
+			$this->field_name, $this->field_value, $this->field_original_value,
+			$this->permalink_options, $this->acf_field_options, $this->wp_post
+		);
 
 		// then.
-		$this->assertFalse( $context->is_terminated());
+		$this->assertFalse( $context->is_terminated() );
 	}
 
 	/**
@@ -101,28 +111,28 @@ class Field_Permalink_Formatter_Context_UnitTest extends PHPUnit_Framework_TestC
 	 *
 	 * @var array
 	 */
-	private $field_value = array('some_value');
+	private $field_value = array( 'some_value' );
 
 	/**
 	 * Field original value.
 	 *
 	 * @var array
 	 */
-	private $field_original_value = array('some_original_value');
+	private $field_original_value = array( 'some_original_value' );
 
 	/**
 	 * Permalink options.
 	 *
 	 * @var array
 	 */
-	private $permalink_options = array('some_permalink_options');
+	private $permalink_options = array( 'some_permalink_options' );
 
 	/**
 	 * ACF field options.
 	 *
 	 * @var array
 	 */
-	private $acf_field_options = array('some_acf_field_options');
+	private $acf_field_options = array( 'some_acf_field_options' );
 
 	/**
 	 * Test post.
