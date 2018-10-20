5fb43efe9082af4635ebe9ea622307b7036f064a
Athlan <athlan@vgroup.pl>
2018-10-20 21:08:58 +0200

    phpcs

--- test/unit/Field_Permalink_Formatter_Context_UnitTest.php
+++ test/unit/Field_Permalink_Formatter_Context_UnitTest.php
@@ -26,8 +26,12 @@ class Field_Permalink_Formatter_Context_UnitTest extends PHPUnit_Framework_TestC
 
 		// when.
 		$context->initialize(
-			$this->field_name, $this->field_value, $this->field_original_value,
-			$this->permalink_options, $this->acf_field_options, $this->wp_post
+			$this->field_name,
+			$this->field_value,
+			$this->field_original_value,
+			$this->permalink_options,
+			$this->acf_field_options,
+			$this->wp_post
 		);
 
 		// then.
